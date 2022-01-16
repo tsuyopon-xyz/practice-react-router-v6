c43f6a4fb7937e7182e1b091f691662e089060b2
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-01-16 23:42:08 +0900

    Done Reading URL Params.

--- src/routes/invoices.tsx
+++ src/routes/invoices.tsx
@@ -1,11 +1,31 @@
+import { Link, Outlet } from 'react-router-dom';
 import { Layout } from 'src/components/Layout';
+import { getInvoices } from 'src/data/invoices';
 
 export const Invoices = () => {
+  const invoices = getInvoices();
+
   return (
     <Layout>
-      <main style={{ padding: '1rem 0' }}>
-        <h2>Invoices</h2>
-      </main>
+      <div style={{ display: 'flex' }}>
+        <nav
+          style={{
+            borderRight: 'solid 1px',
+            padding: '1rem',
+          }}
+        >
+          {invoices.map((invoice) => (
+            <Link
+              style={{ display: 'block', margin: '1rem 0' }}
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </Link>
+          ))}
+        </nav>
+        <Outlet />
+      </div>
     </Layout>
   );
 };
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './home';
 import { Expenses } from './expenses';
 import { Invoices } from './invoices';
+import { Invoice } from './invoice';
 
 export const AppRouter = () => {
   return (
@@ -15,8 +16,19 @@ export const AppRouter = () => {
         {/* https://reactrouter.com/docs/en/v6/getting-started/tutorial#nested-routes */}
         <Route path="/" element={<Home />}>
           <Route path="expenses" element={<Expenses />} />
-          <Route path="invoices" element={<Invoices />} />
+          <Route path="invoices" element={<Invoices />}>
+            <Route path=":invoiceId" element={<Invoice />} />
+          </Route>
         </Route>
+
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
